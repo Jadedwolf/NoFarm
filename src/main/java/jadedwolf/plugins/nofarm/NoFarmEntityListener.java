7addb467fd3a7d9c14211276e23dd3487936b0a1
Jadedwolf <jadedwolf@jadedwolf.com>
2012-05-29 03:50:52 -0400

    Added a start of a XP farm reduction function. Added that mobs do not take fall damage. This change may or may not stay. Pushed Version to 0.3.8

--- src/main/java/jadedwolf/plugins/nofarm/NoFarmEntityListener.java
+++ src/main/java/jadedwolf/plugins/nofarm/NoFarmEntityListener.java
@@ -1,10 +1,12 @@
 package jadedwolf.plugins.nofarm;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 
 
@@ -16,8 +18,18 @@ public class NoFarmEntityListener implements Listener {
   @EventHandler
   public void onEntityDeath(EntityDeathEvent event) {
     if(!(event.getEntity() instanceof Player) && !(event.getEntity().getLastDamageCause() instanceof EntityDamageByEntityEvent)) {
-            event.getDrops().clear();
+        event.setDroppedExp(0);
+        event.getDrops().clear();
         }
   }
-  
+    // Stop mobs from taking fall damge
+  @EventHandler
+  public void onEntityDamageMob(EntityDamageEvent event)
+  {
+		Entity e = event.getEntity();
+    if ((!(e instanceof Player)) && 
+		    (e.getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.FALL)) {
+			event.setDamage(0);
+		}
+  }
 }
